7421dce1296c04300a97f9c1779122e6ac79a72b
Adrien Schuler <adrien.schuler@gmail.com>
2011-10-27 18:28:51 +0200

    update

--- app/register.php
+++ app/register.php
@@ -1,14 +1,15 @@
-<div id="login-container">
+<div id="register-container">
 	<form action="#" method="post">
 		<p><input type="text" name="user[login]" placeholder="Login"></p>
 		<p><input type="password" name="user[password]" placeholder="Mot de passe"></p>
-		<p><input type="submit" name="submit" value="Connexion" class="btn"></p>
+		<p><input type="password" name="user[confirmation]" placeholder="Confirmation"></p>
+		<p><input type="submit" name="submit" value="Inscription" class="btn"></p>
 	</form>
 </div>
 
 <?php
 	if (isset($_POST['submit'])) {
 		$user = new User();
-		$user->login($_POST['user']);
+		$user->create($_POST['user']);
 	}
 ?>
--- app/user.class.php
+++ app/user.class.php
@@ -5,9 +5,37 @@ class User {
 	var $id;
 	var $login;
 	var $password;
+	var $file;
 
 
 	function __construct() {
+		$this->file = simplexml_load_file(XML_USERS);
+	}
+
+	function check_registration($user) {
+		
+		if ($user['password'] === $user['confirmation']) {
+			
+		}
+
+	}
+
+	function check_login($user) {
+		$login = $user['login'];
+		$password = sha1($user['password']);
+
+		$user = $this->file->xpath("/user[@login='$login' and @password='$password']");
+		
+	}
+
+	function create($user) {
+		$date = date('Y-m-d H:i:s');
+		$this->login = $user['login'];
+		$this->password = sha1($user['password']);
+
+	}
+
+	function login($user) {
 		
 	}
 
